82e3b884ae25dc5b5380c7c0249fd8cdd76bd913
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2020-02-12 23:34:12 -0500

    started the files

---
+++ js-game-lab-1.js
@@ -0,0 +1,8 @@
+const play = confirm('Would you like to battle the Almighty Grant?');
+
+if (play == true) {
+	const name = prompt('You seem like a brave soul. What is your name?');
+	console.log(name);
+} else {
+	alert('Run away you COWARD!!!');
+}
