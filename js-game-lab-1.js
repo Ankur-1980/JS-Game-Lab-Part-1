9c1bb3ac4a80d3b549e389901aa78ca3366afc8a
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2020-02-13 11:34:49 -0500

    done!

---
+++ js-game-lab-1.js
@@ -0,0 +1,40 @@
+function startGame() {
+	// start the game
+	const newGame = confirm('Would you like to play a game?');
+	// user name
+	if (newGame == true) {
+		const userName = prompt('What is your name?');
+		// basic game values
+		let userHP = 40;
+		let grantHP = 10;
+		let wins = 0;
+		// battle
+		console.log(`${userName} starts off with ${userHP} \n Grant starts off with ${grantHP}`);
+
+		while (userHP > 0 && wins < 3) {
+			userHP -= Math.floor(Math.random() * 2 + 1);
+			grantHP -= Math.floor(Math.random() * 2 + 1);
+			console.log(`${userName} has ${userHP} remaining`);
+			console.log(`Grant has ${grantHP} remaining`);
+			if (grantHP <= 0) {
+				console.log(`I'm not dead yet, let's do this again!`);
+				grantHP = 10;
+				wins++;
+			}
+		}
+		// if you win
+		if (wins === 3) {
+			console.log(`Nope, I am really dead this time \n ${userName} is VICTORIOUS!!!`);
+		}
+
+		// if you lose
+		else {
+			console.log(`${userName}, you lose, you get nothing, good day sir!`);
+		}
+
+		// if you say no to a new game
+	} else {
+		alert('Run away COWARD!!!');
+	}
+}
+startGame();
